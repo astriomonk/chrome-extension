cfce78cc5b200aff4c580d44e6026069839debed
astriomonk <emmettemm15@gmail.com>
2026-09-12 16:47:27 -0500

    Fix YouTube image replacement content script

--- Content.js
+++ Content.js
@@ -1,18 +1,32 @@
-let kittttyImages = [
+const kittttyImages = [
   "https://placecats.com/300/200",
   "https://placecats.com/neo/300/200",
   "https://placecats.com/millie/300/200",
   "https://placecats.com/millie_neo/300/200",
-"https://placecats.com/neo_banana/300/200",
-"https://placecats.com/neo_2/300/200",
+  "https://placecats.com/neo_banana/300/200",
+  "https://placecats.com/neo_2/300/200",
   "https://placecats.com/bella/300/200",
   "https://placecats.com/poppy/300/200",
-  "https://placecats.com/louie/300/200",
-  ];
+  "https://placecats.com/louie/300/200"
+];
 
-const imgs = document.getElementByTagName("img");
+function replaceImages() {
+  const imgs = document.getElementsByTagName("img");
 
-for (image of imgs) {
-const index  = Math.floor(Math.random() * kittttyImages.length);
-  image.src = kittttyImages[index];
+  for (const image of imgs) {
+    if (image.dataset.kittttyReplaced === "true") continue;
+
+    const index = Math.floor(Math.random() * kittttyImages.length);
+    image.src = kittttyImages[index];
+    image.dataset.kittttyReplaced = "true";
+  }
 }
+
+replaceImages();
+
+// YouTube loads and replaces images dynamically, so keep watching for new ones.
+const observer = new MutationObserver(() => replaceImages());
+observer.observe(document.documentElement, {
+  childList: true,
+  subtree: true
+});
