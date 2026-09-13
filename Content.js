6d5c081eb4dd16f768aab794cc39f2a63188c49e
astriomonk <emmettemm15@gmail.com>
2026-09-12 22:26:10 -0500

    Fix kitty image list syntax and add uploaded kitty thumbnail

--- Content.js
+++ Content.js
@@ -7,8 +7,8 @@ const kittttyImages = [
   "https://placecats.com/neo_2/300/200",
   "https://placecats.com/bella/300/200",
   "https://placecats.com/poppy/300/200",
-  "https://placecats.com/louie/300/200"
-  "https://i.ibb.co/ccfqvdXs/1000004135.jpg",
+  "https://placecats.com/louie/300/200",
+  "https://i.ibb.co/ccfqvdXs/1000004135.jpg"
 ];
 
 const catUrls = new Set(kittttyImages);
